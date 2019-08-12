ffbd5fe08052d1fe691e5a0d29862e5b74960b43
spacejunkblues <spacejunkblues@gmail.com>
2019-08-12 12:56:21 -0500

    First Commit

---
+++ MTR_SCHEDULER_CAPSTONE/MTR_SCHEDULER_CAPSTONE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MTR_SCHEDULER_CAPSTONE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
